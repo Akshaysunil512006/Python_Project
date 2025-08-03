47236ffc5a31d23dad269dea6c4fbf68f146d410
Akshaysunil512006 <akshaysunil512006@gmail.com>
2025-08-03 21:47:52 +0530

    Add files via upload

---
+++ eight.py
@@ -0,0 +1,13 @@
+# Write a Python Program to check prime number
+
+num = int(input("Enter a number: "))
+
+if num > 1:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            print(num, "is NOT a Prime Number")
+            break
+    else:
+        print(num, "is a Prime Number")
+else:
+    print(num, "is NOT a Prime Number")
